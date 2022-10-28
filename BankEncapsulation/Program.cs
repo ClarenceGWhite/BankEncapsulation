ebf3f0ae9a1c91701a06985496b51049ca20b903
clarence <cgwhite76@gmail.com>
2022-10-28 13:27:55 -0500

    Final Version of Bank Encapsulation

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to TrueCoders National Bank.");
+            
+            var cAccount = new BankAccount(100);
+            Console.WriteLine($"Current Balance is: {cAccount.GetBalance()}");
+
+            Console.WriteLine();
+            Console.WriteLine("Enter the amount of your deposit...");
+            double depAmount = int.Parse(Console.ReadLine()); 
+            cAccount.Deposit(depAmount);
+            Console.WriteLine();
+            Console.WriteLine($"Your New Balance is: {cAccount.GetBalance()}");
+            Console.WriteLine("Thanks for visitng.");
+
+           
         }
     }
 }
